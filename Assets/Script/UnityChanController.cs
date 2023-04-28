5a878d078b6a286e569d57135d937bcb53c5e981
lawsuit3310 <aimeet5480@gmail.com>
2023-04-28 10:13:04 +0900

    2023/04/28

---
+++ Assets/Script/UnityChanController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+namespace Script
+{
+    public class UnityChanController : MonoBehaviour
+    {
+        public float rotationSpeed = 360f;   
+        public float moveSpeed = 5f;         
+        public void Update()
+        {
+            Vector3 dir = new Vector3(
+                Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")
+            );
+            if (dir.sqrMagnitude > 0.01f)
+            {
+                Vector3 forward = Vector3.Slerp(transform.forward, dir,
+                    rotationSpeed * Time.deltaTime);
+            }
+        }
+    }
+}
